76f1d6ee731e235212de70284f304e8992a90a06
vsmirnov91 <valentin.smirnoff2016@gmail.com>
2024-11-30 21:02:56 +0500

    Add files via upload

---
+++ Module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as hh
+from true_math import divide as kk
+
+result1 = hh(69, 3)
+result2 = hh(3, 0)
+result3 = kk(49, 7)
+result4 = kk(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+from pdb import runctx
+
+
+def divide(first, second):
+    if second != 0:
+        return first/second
+    if second == 0:
+        return inf
